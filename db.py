bf2b70773a8e1fb34818cf3955f7e194c28155fd
kostuxa <kostuxa@icloud.com>
2023-02-06 23:45:35 +0300

    создан requiremets.txt docker-compose для более удобного разворачивания базы

--- db.py
+++ db.py
@@ -2,6 +2,6 @@ from tortoise import Tortoise
 
 async def init_db():
     await Tortoise.init(
-        db_url='postgres://admin:admin@localhost:5432/tammytanuka',
+        db_url='postgres://super_admin:admin@localhost:5432/tammytanuka',
         modules={'models': ['models']}
     )
